44a77d93dd293343b940c55226f02a64dfd403e9
emersongcardona <emersongcardona@gmail.com>
2021-09-22 12:40:30 -0600

    test crear carro: datos fijos

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,10 +1,10 @@
 # inside 'car' "#create"'
 require 'rails_helper'
-RSpec.describe Cars do 
+RSpec.describe Car do 
     pending "testing Car class"
     it "create a car" do
-        driver = Driver.create(name: "Benjamin")
-        car = Car.create(model: 2014, brand:"mazda", drivers_id: driver.id )
-        expect(car.find).to eq "2014"
+        driver = Driver.new(name: "Benjamin")
+        car = Car.new(model: 2014, brand:"mazda", drivers_id: driver.id )
+        expect(car.model).to eq 2014
     end
 end

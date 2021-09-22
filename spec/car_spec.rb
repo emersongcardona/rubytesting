e3d9bdd4b8dded9d496c47939004c472dedfd9af
emersongcardona <emersongcardona@gmail.com>
2021-09-22 12:26:04 -0600

    ambientes de desarrollo y tablas desplegadas

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Cars do 
     pending "testing Car class"
     it "create a car" do
-        car = Car.create(model: 2014, brand:"mazda", drivers_id: 1 )
+        driver = Driver.create(name: "Benjamin")
+        car = Car.create(model: 2014, brand:"mazda", drivers_id: driver.id )
+        expect(car.find).to eq "2014"
     end
 end
